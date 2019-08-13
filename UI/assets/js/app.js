9228af490691b88dcc2bb34f5cca6d7d2c68bec4
ebundala <ebundala@gmail.com>
2019-08-13 08:03:47 +0300

    feature(mentors-page):add navigation bar  and page layout [Start #167736117]

--- UI/assets/js/app.js
+++ UI/assets/js/app.js
@@ -1,20 +1,38 @@
 
-
-/**
- * @description app main entry point
- */
-const main = () => {
-// console.debug('app loaded hooray');
-};
 /**
 * @desc utility function to query the dom
 * @param {String} selector - a css selector to query
 * @param {boolean } all - a flag to wether query one node or all nodes
 * @returns {HTMLElement } return - a dom element
 */
-/* const $ = (selector, all) => (all
+const $ = (selector, all) => (all
   ? document.querySelectorAll(selector) : document.querySelector(selector));
-*/
+
+/**
+ * @description show or hide sidebar
+ */
+const toggleSideBar = () => {
+  const sidebar = $('.menu');
+  if (sidebar) {
+    if (!sidebar.classList.contains('menu-hide')) {
+      sidebar.classList.add('menu-hide');
+      sidebar.classList.remove('menu-show');
+    } else {
+      sidebar.classList.add('menu-show');
+      sidebar.classList.remove('menu-hide');
+    }
+  }
+};
+
+
+/**
+ * @description app main entry point
+ */
+const main = () => {
+  // console.debug('app loaded hooray');
+  $('.menu-icon').addEventListener('click', toggleSideBar);
+};
+
 /**
  * @desc listens for dom loading complete
  */
